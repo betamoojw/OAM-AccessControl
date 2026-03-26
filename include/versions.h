f7bf1b60191cada4f4ebfb4613605aed4d7f8266
Ing-Dom <dom@ing-dom.de>
2026-03-26 10:33:35 +0100

    wip

--- include/versions.h
+++ include/versions.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define MAIN_Version "7f88051"
+#define MAIN_Version "d11472d"
 #define KNX_Version "2.3.1+7124a64"
-#define MODULE_AccessControl_Version "0.11.0+5f6aea0"
+#define MODULE_AccessControl_Version "0.11.0+d612ad6"
 #define MODULE_AccessControl_Version_Major 0
 #define MODULE_AccessControl_Version_Minor 11
 #define MODULE_AccessControl_Version_Revision 0
